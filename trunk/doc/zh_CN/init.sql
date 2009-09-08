680e7ebaa1c0a15875b9b5a30fd800ba67f602a9
leeaee <leeaee@d5b54fc0-8d74-11de-92d6-5bd20ad860ee>
2009-09-08 15:40:23 +0000

    完善Spring Security权限控制的底层.

--- trunk/doc/zh_CN/init.sql
+++ trunk/doc/zh_CN/init.sql
@@ -8,7 +8,7 @@ insert into cs_role (id, name) values (1, '超级管理员');
 insert into cs_role (id, name) values (2, '游客');
 
 
--- cs_authority
+-- cs_authority 
 insert into cs_authority (id, name, display_name) values (1, 'a_browse_index', '主页浏览');
 
 insert into cs_authority (id, name, display_name) values (2, 'a_browse_admin', '管理员浏览');
@@ -23,19 +23,19 @@ insert into cs_authority (id, name, display_name) values (9, 'a_delete_role', '�
 
 
 -- cs_resource
-insert into cs_resource (id, resource_type, application, value, position) values(1, 'url', 'csadmin', '/index', 1.0);
+insert into cs_resource (id, resource_type, application, value, position) values(1, 'url', 'csadmin', '/index*', 1.0);
 
-insert into cs_resource (id, resource_type, application, value, position) values(2, 'url', 'csadmin', '/admin', 2.0);
-insert into cs_resource (id, resource_type, application, value, position) values(3, 'url', 'csadmin', '/admin?method=browse*', 2.0);
-insert into cs_resource (id, resource_type, application, value, position) values(4, 'url', 'csadmin', '/admin?method=create*', 2.1);
-insert into cs_resource (id, resource_type, application, value, position) values(5, 'url', 'csadmin', '/admin?method=update*', 2.2);
-insert into cs_resource (id, resource_type, application, value, position) values(6, 'url', 'csadmin', '/admin?method=delete*', 2.3);
+insert into cs_resource (id, resource_type, application, value, position) values(2, 'url', 'csadmin', '/admin?method=browse*', 2.0);
+insert into cs_resource (id, resource_type, application, value, position) values(3, 'url', 'csadmin', '/admin?method=create*', 3.0);
+insert into cs_resource (id, resource_type, application, value, position) values(4, 'url', 'csadmin', '/admin?method=update*', 4.0);
+insert into cs_resource (id, resource_type, application, value, position) values(5, 'url', 'csadmin', '/admin?method=delete*', 5.0);
+insert into cs_resource (id, resource_type, application, value, position) values(6, 'url', 'csadmin', '/admin*', 6.0);
 
-insert into cs_resource (id, resource_type, application, value, position) values(7, 'url', 'csadmin', '/role', 3.0);
-insert into cs_resource (id, resource_type, application, value, position) values(8, 'url', 'csadmin', '/role?method=browse*', 3.0);
-insert into cs_resource (id, resource_type, application, value, position) values(9, 'url', 'csadmin', '/role?method=create*', 3.1);
-insert into cs_resource (id, resource_type, application, value, position) values(10, 'url', 'csadmin', '/role?method=update*', 2.2);
-insert into cs_resource (id, resource_type, application, value, position) values(11, 'url', 'csadmin', '/role?method=delete*', 3.3);
+insert into cs_resource (id, resource_type, application, value, position) values(7, 'url', 'csadmin', '/role?method=browse*', 7.0);
+insert into cs_resource (id, resource_type, application, value, position) values(8, 'url', 'csadmin', '/role?method=create*', 8.0);
+insert into cs_resource (id, resource_type, application, value, position) values(9, 'url', 'csadmin', '/role?method=update*', 9.0);
+insert into cs_resource (id, resource_type, application, value, position) values(10, 'url', 'csadmin', '/role?method=delete*', 10.0);
+insert into cs_resource (id, resource_type, application, value, position) values(11, 'url', 'csadmin', '/role*', 11.0);
 
 
 -- cs_admin_x_role
@@ -60,13 +60,16 @@ insert into cs_role_x_authority values(2, 6);
 
 -- cs_resource_x_authority
 insert into cs_resource_x_authority values(1, 1);
+
 insert into cs_resource_x_authority values(2, 2);
-insert into cs_resource_x_authority values(3, 2);
-insert into cs_resource_x_authority values(4, 3);
-insert into cs_resource_x_authority values(5, 4);
-insert into cs_resource_x_authority values(6, 5);
+insert into cs_resource_x_authority values(3, 3);
+insert into cs_resource_x_authority values(4, 4);
+insert into cs_resource_x_authority values(5, 5);
+insert into cs_resource_x_authority values(6, 2);
+
 insert into cs_resource_x_authority values(7, 6);
 insert into cs_resource_x_authority values(8, 7);
-insert into cs_resource_x_authority values(9, 7);
-insert into cs_resource_x_authority values(10, 8);
-insert into cs_resource_x_authority values(11, 9);
+insert into cs_resource_x_authority values(9, 8);
+insert into cs_resource_x_authority values(10, 9);
+insert into cs_resource_x_authority values(11, 6);
+
